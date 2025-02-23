7a07515c0d8779cf2b2d9f1ef0e11e6419bbb519
Manuek <manuelbena94@gmail.com>
2025-02-23 09:11:39 +0100

    add hilt

--- app/src/main/java/com/example/agendia/MainActivity.kt
+++ app/src/main/java/com/example/agendia/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.agendia.ui.theme.AgendiaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
